0b1d925feb0c5787b8b55c4127151eca857181e6
buloshka <79216709+buloshka@users.noreply.github.com>
2026-07-20 17:20:50 +0300

    jinja2 utils

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 
+from src.utils.templates import source
 
-templates = Jinja2Templates(directory='src/templates')
 
 app = FastAPI(
     title='Price Tracker',
@@ -11,7 +10,7 @@ app = FastAPI(
 
 @app.get(path='/', response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse(
+    return source.TemplateResponse(
         request=request,
         name='index.html',
         context={'my_custom_id': 2}
---
+++ src/utils/templates.py
@@ -0,0 +1,4 @@
+from fastapi.templating import Jinja2Templates
+
+
+source = Jinja2Templates(directory='src/templates')
